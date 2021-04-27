45484268e622acb19749d0810431611e4addfebf
genhcode99 <genhcode99@gmail.com>
2021-04-28 08:23:44 +0900

    #15.2 Get Requests Part Two
    working

--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
+//app 선언
+
 import express from "express";
 
 const PORT = 4000;
 
 const app = express();
 
-const handleListening = () => console.log(`✅ Server listening on port ${PORT} ⛴`);
+
+
+//app 설정
+
+const handleHome = () => console.log("Somebody is tring to go home.");
+
+app.get("/", handleHome);
+
+
+
+
+//외부 접속 listen
+
+const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} ⛴`);
 
 app.listen(PORT, handleListening);
