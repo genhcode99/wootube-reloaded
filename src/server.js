34e539e2e7e0a3c42c348fd1e6997a657ed2a45e
genhcode99 <genhcode99@gmail.com>
2021-04-28 21:41:51 +0900

    #16.2 Cleaning the Code

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
-//app 선언
-
+//-----< Express app 선언 >-----
 import express from "express";
 
 const PORT = 4000;
@@ -10,30 +9,22 @@ const app = express();
 //-----< MIDDLE WARE >-----
 import logger from "morgan";
 
+
 app.use(logger("dev"));
 
-//-----< CONTROLLER >-----
-const globalRouter = express.Router();
-const handleHome = (req, res) => res.send("home");
-globalRouter.get("/", handleHome);
 
-const videoRouter = express.Router();
-const handelWatchVideo = (req, res) => res.send("Watch Video");
-videoRouter.get("/watch", handelWatchVideo);
+//-----< Router setting >-----
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter"
+import userRouter from "./routers/userRouter"
 
-const userRouter = express.Router();
-const handelEditUser = (req, res) => res.send("Edit User");
-userRouter.get("/edit", handelEditUser);
 
-app.use("/", globalRouter)
+app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-// app.get("/", handleHome);
-// app.get("/login", handleLogin);
 
-//외부 접속 listen
-
+//-----< 외부 request 를 듣게 준비하고 있어라 >-----
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} ⛴`);
 
 app.listen(PORT, handleListening);
