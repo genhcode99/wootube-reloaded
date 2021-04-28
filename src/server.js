24a6a80076803303a9ae1e37dd9a51484648a5ff
genhcode99 <genhcode99@gmail.com>
2021-04-28 19:36:04 +0900

    #15.10 Middleware Recap

--- src/server.js
+++ src/server.js
@@ -11,19 +11,30 @@ const app = express();
 //----------< app 설정 >----------
 
 //MIDDLE WARE
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url} `);
+const methodLogger = (req, res, next) => {
+  console.log("METHOD", req.method);
+  next();
+};
+const routerLogger = (req, res, next) => {
+  console.log("PATH", req.path);
   next();
 };
 
-app.use(logger);
+app.use(methodLogger, routerLogger);
 
 
 //HANDLER
-const handleHome = (req, res) => res.send("Hello!");
+const handleHome = (req, res) => {
+  console.log("I will respond.");
+  return res.send("Hello!");
+};
+
+const login = (req, res) => {
+  return res.send("login");
+}
 
 app.get("/", handleHome);
-
+app.get("/login", login);
 
 //외부 접속 listen
 
