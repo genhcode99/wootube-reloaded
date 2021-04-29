3a6c82b58b5818c71f94b956b003e18f4b02d880
genhcode99 <genhcode99@gmail.com>
2021-04-29 21:43:52 +0900

    #17.8 Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,17 @@
-const fakeUser = {
-  username:"JeU",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Hello",
+    },
+    {
+      title: "Video #2"
+    },
+    {
+      title: "What's up"
+    }
+  ];
+  return res.render("home", {pageTitle: "Home", videos})
 };
-
-
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser});
 export const search = (req, res) => res.send("search");
 
 export const upload = (req, res) => res.send("Upload");
