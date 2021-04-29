0097b22fb82c23dd1ac2f6b3347fb622837f9aa2
genhcode99 <genhcode99@gmail.com>
2021-04-29 20:05:09 +0900

    # 17.4 Variable to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,4 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
 export const search = (req, res) => res.send("search");
 
 
