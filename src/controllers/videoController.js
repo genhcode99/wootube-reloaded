7456e7b39e14a2363f5f96ee29941eb436dbc52b
genhcode99 <genhcode99@gmail.com>
2021-04-30 10:10:35 +0900

    #18.0 Array Datebase part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,35 +1,44 @@
-export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating:5,
-      comments:2,
-      createdAt: "2minutes ago",
-      views:59,
-      id:1,
-    },
-    {
-      title: "Second Video",
-      rating:5,
-      comments:2,
-      createdAt: "2minutes ago",
-      views:59,
-      id:2,
-    },
-    {
-      title: "Third Video",
-      rating:5,
-      comments:2,
-      createdAt: "2minutes ago",
-      views:59,
-      id:3,
-    },
+let videos = [
+  {
+    title: "First Video",
+    rating:5,
+    comments:2,
+    createdAt: "2minutes ago",
+    views:59,
+    id:1,
+  },
+  {
+    title: "Second Video",
+    rating:5,
+    comments:2,
+    createdAt: "2minutes ago",
+    views:59,
+    id:2,
+  },
+  {
+    title: "Third Video",
+    rating:5,
+    comments:2,
+    createdAt: "2minutes ago",
+    views:59,
+    id:3,
+  },
   ];
+  
+export const trending = (req, res) => {
   return res.render("home", {pageTitle: "Home", videos})
 };
+
 export const search = (req, res) => res.send("search");
 
-export const upload = (req, res) => res.send("Upload");
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => {
+  const {id} = req.params;
+  const video = videos[id-1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`});
+};
+
 export const edit = (req, res) => res.render("edit");
+
 export const deleteVideo = (req, res) => res.send ("Delete Video")
+
+export const upload = (req, res) => res.send("Upload");
