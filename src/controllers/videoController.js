973c0bf397292bbdb8e119c8b2ea64f844190270
genhcode99 <genhcode99@gmail.com>
2021-04-30 18:34:41 +0900

    #18.2 Edit Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -29,16 +29,17 @@ export const trending = (req, res) => {
   return res.render("home", {pageTitle: "Home", videos})
 };
 
-export const search = (req, res) => res.send("search");
 
 export const watch = (req, res) => {
   const {id} = req.params;
   const video = videos[id-1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video});
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video});
 };
 
-export const edit = (req, res) => res.render("edit");
+export const getEdit = (req, res) => {
+  const {id} = req.params;
+  const video = videos[id-1];
+  return res.render("edit", {pageTitle:`Editing: ${video.title}`, video});
+};
 
-export const deleteVideo = (req, res) => res.send ("Delete Video")
-
-export const upload = (req, res) => res.send("Upload");
+export const postEdit = (req, res) => { };
