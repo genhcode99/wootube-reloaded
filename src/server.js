76102cfe0738decd549a600065db3564bbade615
genhcode99 <genhcode99@gmail.com>
2021-05-06 14:12:56 +0900

    #19.7 Sessions and Cookies part One

--- src/server.js
+++ src/server.js
@@ -15,8 +15,15 @@ app.set("views", process.cwd() + "/src/views");
 
 //-----< MIDDLE WARE >-----
 import logger from "morgan";
-
+import session from "express-session";
+      //Logger
 app.use(logger("dev"));
+      //세션 (로그인 쿠키 등을 제어)  
+app.use(session({
+  secret: "Hello!",
+  resave: true,
+  saveUninitialized: true,
+}))
 
   //  Express가 form 태그에서 제출된 정보를 자바스크립트로 이해시키는 방법의 미들웨어
 app.use(express.urlencoded({extended: true}));
