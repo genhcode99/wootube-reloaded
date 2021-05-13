482d6aa82703813c743f4e2ffaf28e9454bf5e15
JeUpark <genhcode@gmail.com>
2021-05-13 20:46:36 +0900

    #21.2 Webpack Configuration part Two

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
 // 완전 오래된 자바스크립트 코드만 이해할 수 있다!
+const path = require("path");
 
 
 module.exports = {
   entry: "./src/client/js/main.js",
+  mode: "development",
   output: {
     filename: "main.js",
-    path: "./assets/js",
+    path: path.resolve(__dirname, "assets", "js"),
   },
-
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [
+            ['@babel/preset-env', {
+              targets: "defaults"
+            }]
+          ]
+        }
+      }
+    }]
+  }
 };
