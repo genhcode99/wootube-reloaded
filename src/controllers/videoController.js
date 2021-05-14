02891123765290c8bf6f47d53893c8b780db1375
JeUpark <genhcode@gmail.com>
2021-05-14 14:12:17 +0900

    #22.1 Styles part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,7 +8,7 @@ export const home = async (req, res) => {
 
   const videos = await Video.find({}).sort({
     createdAt: "desc"
-  });
+  }).populate("owner");
   return res.render("home", {
     pageTitle: "Home",
     videos
