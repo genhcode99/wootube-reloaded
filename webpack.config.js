e1ff6b2ba6d3c45d797ea9a1c0426ea4074e708f
JeUpark <genhcode@gmail.com>
2021-05-14 09:26:47 +0900

    #21.4 SCSS Loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,17 +11,23 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.js$/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: [
-            ['@babel/preset-env', {
-              targets: "defaults"
-            }]
-          ]
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: "defaults"
+              }]
+            ]
+          }
         }
-      }
-    }]
+      },
+      {
+        test: /\.scss$/,
+        //마지막에 사용될 로더 부터  처음 사용할 로더 까지 반대 순서로 적어야 한다.(웹팩은 아래부터 읽어나감.)
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+    ]
   }
 };
