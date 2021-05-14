213d9564e354c860b68f3c64fa7e0038a7808b37
JeUpark <genhcode@gmail.com>
2021-05-14 09:52:37 +0900

    #21.5 MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 // 완전 오래된 자바스크립트 코드만 이해할 수 있다!
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require("path");
 
 
 module.exports = {
   entry: "./src/client/js/main.js",
   mode: "development",
+  plugins: [new MiniCssExtractPlugin({
+    filename: "css/styles.css",
+  })],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
   },
   module: {
     rules: [{
@@ -26,7 +30,7 @@ module.exports = {
       {
         test: /\.scss$/,
         //마지막에 사용될 로더 부터  처음 사용할 로더 까지 반대 순서로 적어야 한다.(웹팩은 아래부터 읽어나감.)
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       },
     ]
   }
