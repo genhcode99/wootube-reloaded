668fd80cc72d75d0c8d81ad80a33ae4d575b64fd
JeUpark <genhcode@gmail.com>
2021-05-15 11:20:31 +0900

    #23.2 Mute and Unmute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,9 @@
+//-----< Import to World >----
 const video = document.querySelector("video")
-const playBtn = document.getElementById("play");
-const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+
+//-----< Play and Pause >-----
+const playBtn = document.getElementById("play");
 
 const handlePlayClick = (e) => {
   if (video.paused) {
@@ -10,15 +11,26 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
-
-const handleMute = (e) => {};
-
-
 
 playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("click", handlePlay);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+
+
+//-----< Sound >-----
+const muteBtn = document.getElementById("mute");
+const volumeRange = document.getElementById("volume");
+// # mute
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
+};
+
+muteBtn.addEventListener("click", handleMute);
+
+// # volume controll
