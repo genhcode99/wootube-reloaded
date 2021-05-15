7803f80bdbcbf77daf5ee1a74661d01864b26844
JeUpark <genhcode@gmail.com>
2021-05-15 12:04:41 +0900

    #23.3 Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,10 +2,14 @@
 const video = document.querySelector("video")
 const time = document.getElementById("time");
 
+
+
 //-----< Play and Pause >-----
+// # import
 const playBtn = document.getElementById("play");
 
-const handlePlayClick = (e) => {
+// # play and pause
+const handlePlayClick = (event) => {
   if (video.paused) {
     video.play();
   } else {
@@ -17,20 +21,37 @@ const handlePlayClick = (e) => {
 playBtn.addEventListener("click", handlePlayClick);
 
 
+
 //-----< Sound >-----
+// # import
 const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 // # mute
-const handleMute = (e) => {
+const handleMute = (event) => {
   if (video.muted) {
     video.muted = false;
   } else {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
 muteBtn.addEventListener("click", handleMute);
 
 // # volume controll
+const handleVolumeChange = (event) => {
+  const value = event.target.value;
+
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
+
+volumeRange.addEventListener("input", handleVolumeChange);
