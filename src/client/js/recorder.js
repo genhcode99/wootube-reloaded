0b6e82a1a25b68b46c0b89cf3b91748c29647b33
JeUpark <genhcode@gmail.com>
2021-05-19 12:03:39 +0900

    #26.3 Thumbnail

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,16 @@
+import regeneratorRuntime from "regenerator-runtime";
 import {
   createFFmpeg,
   fetchFile
 } from "@ffmpeg/ffmpeg";
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
 let stream;
 let recorder;
 let videoFile;
+
 
 const handleDownload = async () => {
   const ffmpeg = createFFmpeg({
@@ -17,22 +20,49 @@ const handleDownload = async () => {
 
   ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
 
-  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+  await ffmpeg.run(
+    "-i",
+    "recording.webm",
+    "-r",
+    "60",
+    "output.mp4");
+
+  await ffmpeg.run(
+    "-i",
+    "recording.webm",
+    "-ss", "00:00:01",
+    "-frames:v",
+    "1",
+    "thumbnail.jpg");
 
   const mp4File = ffmpeg.FS("readFile", "output.mp4");
+  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg")
 
   const mp4Blob = new Blob([mp4File.buffer], {
     type: "video/mp4"
   });
+  const thumbBlob = new Blob([thumbFile.buffer], {
+    type: "image/jpg"
+  });
 
   const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbUrl = URL.createObjectURL(thumbBlob);
 
   const a = document.createElement("a");
   a.href = mp4Url;
   a.download = "MyRecording.mp4";
   document.body.appendChild(a);
   a.click();
+  location.reload();
+
+  const thumbA = document.createElement("a");
+  thumbA.href = thumbUrl;
+  thumbA.download = "MyThumbnail.jpg";
+  document.body.appendChild(thumbA);
+  thumbA.click();
+  location.reload();
 };
+
 
 const handleStop = () => {
   startBtn.innerText = "Download Recording";
@@ -40,6 +70,7 @@ const handleStop = () => {
   startBtn.addEventListener("click", handleDownload);
   recorder.stop();
 };
+
 
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
@@ -56,7 +87,9 @@ const handleStart = () => {
     video.play();
   };
   recorder.start();
+
 };
+
 
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
@@ -66,7 +99,7 @@ const init = async () => {
   video.srcObject = stream;
   video.play();
 };
-
 init();
+
 
 startBtn.addEventListener("click", handleStart);
